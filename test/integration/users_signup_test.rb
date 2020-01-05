5cbd71d4e0e29efb9f5530d604930520212e8a55
rubyc <759337828@qq.com>
2020-01-05 12:43:31 +0800

    Implement basic login

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -24,15 +24,16 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   	get signup_path
   	assert_difference 'User.count', 1 do
   		post users_path, params: { user: {
-  			name: "759337828",
-  			email: "759337828@qq.com",
-  			password: "759337828", 
-  			password_confirmation: "759337828"
+  			name: "rubycc",
+  			email: "759337828yc@qq.com",
+  			password: "password", 
+  			password_confirmation: "password"
   		}}
   	end
 
   	follow_redirect!
   	assert_template 'users/show'
+    assert is_logged_in?
   	assert_not flash.empty?
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,5 +13,9 @@ class ActiveSupport::TestCase
 
   include ApplicationHelper
 
+  def is_logged_in?
+  	!session[:user_id].nil?
+  end
+
   # Add more helper methods to be used by all tests here...
 end
