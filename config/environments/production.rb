cfa28f6b7841483a226e4c24c2c4a39c22bed3ed
苑畅 <759337828@qq.com>
2020-01-08 22:01:58 +0800

    email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,6 +89,22 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+
+  # config.action_mailer.default_url_options = { :host => 'protected-peak-56043.herokuapp.com' }
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = config_for(:email).symbolize_keys
+
+  config.action_mailer.default_url_options = { :host => 'protected-peak-56043.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      address: "smtpcloud.sohu.com",
+      port: 25,
+      domain: "NOHgsO3wstJoZ4OQerTqCgY5hOdUOPYQ.sendcloud.org",
+      authentication: "plain",
+      user_name: "yuanchang_test_2bxacM",
+      password: "9KGXXck5poAa9aQg",
+      enable_starttls_auto: true,
+  }
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
   # middleware. The `delay` is used to determine how long to wait after a write
